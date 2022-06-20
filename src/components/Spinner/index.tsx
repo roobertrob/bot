f6342d33a1584985350ca546a57e45e94d58efd5
Robert <robert.fonseca@solides.com.br>
2022-06-20 08:53:19 -0300

    fix: :bug: remove semicolon

--- src/components/Spinner/index.tsx
+++ src/components/Spinner/index.tsx
@@ -3,7 +3,7 @@ import { SpinnerGap } from 'phosphor-react';
 function Spinner() {
     return (
       <div className="flex items-center justify-center rounded bg-white p-8 md:p-5 max-h-max animate-pulse">
-        <SpinnerGap className='animate-spin duration-150 text-orangeWarning h-10 w-10 md:h-20 md:w-20' />;
+        <SpinnerGap className='animate-spin duration-150 text-orangeWarning h-10 w-10 md:h-20 md:w-20' />
       </div>
     );
 }
